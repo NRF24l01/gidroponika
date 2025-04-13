2bbfd6637cf8d0b35b248b4cb71d47945d9d362c
nrf24l01 <s.nevrovsky@ya.ru>
2025-04-13 23:08:57 +0300

    Fixed icons

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,12 +1,12 @@
 <template>
   <!-- Header -->
-  <header class="flex justify-between items-center mb-6 p-4 bg-gray-800">
+  <header class="flex justify-between items-center mb-6 p-4">
     <h1 class="text-xl font-bold flex items-center space-x-2">
       <HomeIcon class="size-7 text-blue-500" />
       <span>Система управления грядкой</span>
     </h1>
     <button class="bg-gray-800 px-4 py-2 rounded-lg hover:bg-gray-700 flex items-center space-x-2">
-      <WrenchIcon class="size-6 text-white" />
+      <CircleStackIcon class="size-6 text-white" />
       <span>Термометры</span>
     </button>
   </header>
@@ -15,7 +15,7 @@
 
 <script setup>
 import {
-  WrenchIcon,
+  CircleStackIcon,
   HomeIcon
 } from "@heroicons/vue/24/outline"
 </script>
